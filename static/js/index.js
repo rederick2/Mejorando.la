58abf1e12780fff546a28dbad8303879ceac131f
Joan Piedra <joan@joanpiedra.com>
2012-05-23 09:43:06 -0400

    Usar 8K en vez de 8,013 en el contador de YouTube

--- static/js/index.js
+++ static/js/index.js
@@ -42,7 +42,7 @@ jQuery(function ($) {
 			rep = rep + ''; // coerce to string
 			if (rep < 1000) {
 				return rep; // return the same number
-			} else if (rep < 10000) { // place a comma between
+			} else if (rep < 5000) { // place a comma between
 				return rep.charAt(0) + ',' + rep.substring(1);
 			} else { // divide and format
 				return (rep/1000).toFixed(rep % 1000 != 0)+'k';
@@ -54,12 +54,12 @@ jQuery(function ($) {
 			var count = $.cookie('youtube_views');
 			var $youtube = $('.btn_youtube');
 			if (count) {
-				$youtube.find('.count').text(count + ' views');
+				$youtube.find('.count').text(count);
 			} else {
 				$.getJSON('http://gdata.youtube.com/feeds/api/users/' + $youtube.data('id') + '?alt=json&callback=?', function(data){ 
 					var count = formatNumber(data.entry['yt$statistics'].subscriberCount);
 					$.cookie('youtube_views', count);
-					$youtube.find('.count').text(count + ' views');
+					$youtube.find('.count').text(count);
 				});
 			}
 		})();
